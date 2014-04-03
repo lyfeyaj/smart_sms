066cec0bd24c64911ba5918366b73d3234155a6d
lyfeyaj <lyfeyaj@gmail.com>
2014-04-03 22:48:25 +0800

    add encoding and update readme.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-smart_sms
+SmartSMS 短信认证或校验服务
 =========
 
-smart_sms
+集成了 [云片网络](http://www.yunpian.com) 的短信服务
--- lib/generators/smart_sms/templates/smart_sms_config.rb
+++ lib/generators/smart_sms/templates/smart_sms_config.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 SmartSMS.configure do |config|
   # config.api_key = nil
   # config.api_version = :v1
