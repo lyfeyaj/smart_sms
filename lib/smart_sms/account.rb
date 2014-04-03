0b719c282c6bb8c0aea7856fbdf1e1f5ddfa106f
lyfeyaj <lyfeyaj@gmail.com>
2014-04-03 23:32:02 +0800

    add comments for codes

--- lib/smart_sms/account.rb
+++ lib/smart_sms/account.rb
@@ -4,11 +4,12 @@ module SmartSMS
   module Account
     extend self
 
+    # 获取用户信息
     def info
       Request.post 'user/get.json'
     end
 
-    # Set account information
+    # 设置用户信息
     #   emergency_contact: 紧急联系人
     #   emergency_mobile:  紧急联系人手机号
     #   alarm_balance:     短信余额提醒阈值。一天只提示一次
--- lib/smart_sms/config.rb
+++ lib/smart_sms/config.rb
@@ -19,15 +19,15 @@ module SmartSMS
 
   class Configuration #:nodoc:
     include ActiveSupport::Configurable
-    config_accessor :api_key
-    config_accessor :api_version
-    config_accessor :template_id
-    config_accessor :template_value
-    config_accessor :page_num
-    config_accessor :page_size
-    config_accessor :company
+    config_accessor :api_key # 授权 API KEY
+    config_accessor :api_version # API 的版本, 当前仅有v1
+    config_accessor :template_id # 指定发送信息时使用的模板
+    config_accessor :template_value # 用于指定信息文本中的可替换内容, 数组形势: [:code, :company]
+    config_accessor :page_num # 获取信息时, 指定默认的页数
+    config_accessor :page_size # 获取信息时, 一页包含信息数量
+    config_accessor :company # 默认公司名称
     config_accessor :default_interval   # 查询短信时的时间段: end_time - start_time
-    config_accessor :store_sms_in_local # true or false
+    config_accessor :store_sms_in_local # 是否存储SMS信息在本地: true or false
     config_accessor :verification_code_algorithm # :simple, :middle, :complex
   end
 
